8c36b2ca7af94fa073da25b93996dc9e50769f31
Erick Baltodano <erickbaltodano@ciencias.unam.mx>
2025-02-02 16:43:41 -0800

    avance2 tarea opcional 1

--- tarea_opcional_1/src/Metodo_Numerico.py
+++ tarea_opcional_1/src/Metodo_Numerico.py
@@ -1,5 +1,25 @@
+from random import *
+
+
 class Metodo_Numerico:
     
     @staticmethod
-    def metodo():
-        pass
+    def busqueda_en_linea(iteraciones,funcion,valores,espacio_de_busqueda):
+        
+        dimension = len(valores)
+        
+        
+        #Generamos una primera solución inicial.
+        """INCOMPLETO"""
+        sol_inicial = 0
+        sol_i = None
+        
+        for i in range(iteraciones):
+            """INCOMPLETO"""
+            sol_i = i
+            
+            
+        return sol_i
+        
+        
+        
--- tarea_opcional_1/src/Rastrigin.py
+++ tarea_opcional_1/src/Rastrigin.py
@@ -1,1 +1,13 @@
-print ("rastrigin")
+from Function import *
+
+dimension =int(input ("Ingrese la dimension sobre la que realizaremos la búsqueda:  "  ))
+espacio_de_busqueda = []
+funcion = Function.rastrigin
+
+for i in range (dimension):
+    espacio_de_busqueda.append(int(input("Ingrese el límite inferior sobre la dimension {}:  ".format(i))))
+    espacio_de_busqueda.append(int(input("Ingrese el límite superior sobre la dimension {}:  ".format(i))))
+    
+    
+print (espacio_de_busqueda)
+print (funcion(1,[50]))
