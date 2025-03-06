246b268e63c9720cb94ad5203cb37ad051e2a67c
TomateTorres <tomatito.verde.frito.420@gmail.com>
2025-03-05 21:44:03 -0600

    Modificaciones a la generadora de vecinos y su respectivo makefile

--- Job_Shop/src/Version_Generadora_Vecinos.py
+++ Job_Shop/src/Version_Generadora_Vecinos.py
@@ -3,6 +3,7 @@ from Vertice_VAde import *
 from Solution_Generator_VAde import *
 from Calculadora_makespan_VAde import *
 from Generadora_de_vecinos import *
+import random
 
 ejemplar = input("Escribe el nombre del ejemplar (no olvides el .txt):\t")
 
@@ -30,9 +31,13 @@ makespan, r, q, t, d, info = Evaluador_Makespan.calculadora_makespan(numero_de_m
 # Construir nueva_info
 nueva_info = Generadora_de_vecinos.construir_nueva_info(makespan, r, q, info)
 
-# Imprimir detalles de las operaciones críticas
-Generadora_de_vecinos.imprimir_nueva_info(nueva_info, makespan)
+'''# Imprimir detalles de las operaciones críticas
+Generadora_de_vecinos.imprimir_nueva_info(nueva_info, makespan)'''
 
-vecindad, movimientos = Generadora_de_vecinos.construir_vecindad(solucion, makespan, r, q, info)
-print(f"\n Los vecinos son: {vecindad}")
+vecindad_de_movimientos = Generadora_de_vecinos.construir_vecindad(solucion, makespan, r, q, info)
+#print(f"\n Los vecinos son: {vecindad_de_movimientos}")
 
+
+idx_vecino = random.randint(0, len(vecindad_de_movimientos)-1) # El índice del vecino elegido.
+solucion_vecina = Generadora_de_vecinos.decodificar_vecino(idx_vecino, vecindad_de_movimientos, solucion)
+makespan_vecino = Generadora_de_vecinos.evaluar_vecino(solucion_vecina, numero_de_maquinas, numero_de_trabajos, lista_de_vertices)
