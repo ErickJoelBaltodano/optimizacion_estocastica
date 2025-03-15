0999d6a51a2f883ed8660570125059e1626900dc
Erick Baltodano <erickbaltodano@ciencias.unam.mx>
2025-03-15 15:20:16 -0700

    Actualizando rama principal (minierrores)

---
+++ Tarea_Opcional_1/src/Busqueda.py
@@ -0,0 +1,14 @@
+class Busqueda:
+    
+    @staticmethod
+    def recocido_simulado(no_de_evaluaciones,tamaño_de_la_poblacion= 40,no_de_bits = 20, ):
+        
+        
+        evaluaciones_actuales = 0
+        
+        
+        while evaluaciones_actuales < tamaño_de_la_poblacion:
+            
+            
+            
+            evaluaciones_actuales += 1 # PENDIENTE
